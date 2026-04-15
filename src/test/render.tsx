3c784f758e669ae45597d98d659c73938ab7f0bf
Tim <timkentdev@gmail.com>
2026-04-15 16:00:21 -0700

    add test setup files

---
+++ src/test/render.tsx
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+
+export function renderWithClient(ui: React.ReactNode, routes: string[]) {
+	return render(
+		<MemoryRouter initialEntries={routes}>{ui}</MemoryRouter>,
+	);
+}
