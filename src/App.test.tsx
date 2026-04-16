d8b43a36c2be1baf8901ca0b62a096605d43b8e6
Tim <timkentdev@gmail.com>
2026-04-16 12:29:16 -0700

    add desktop navigation tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "@/App";
 import { renderWithClient } from "@/test/render";
 
@@ -40,6 +41,70 @@ describe("App root layer", () => {
 
 		expect(
 			screen.getByRole("heading", { name: /404 - page not found/i }),
+		).toBeInTheDocument();
+	});
+});
+
+/**
+ * Test primary navigation elements.
+ */
+describe("Root nav element", () => {
+	const user = userEvent.setup();
+
+	it("navigates from Home to Features using desktop nav", async () => {
+		renderWithClient(<App />, ["/"]);
+
+		await user.click(screen.getByTestId("features-link"));
+		expect(
+			screen.getByRole("heading", { name: "Features" }),
+		).toBeInTheDocument();
+	});
+
+	it("navigates from Features to Films using desktop nav", async () => {
+		renderWithClient(<App />, ["/features"]);
+
+		await user.click(screen.getByTestId("films-link"));
+
+		expect(
+			screen.getByRole("heading", {
+				name: "Films",
+			}),
+		).toBeInTheDocument();
+	});
+
+	it("navigates from Films to People using desktop nav", async () => {
+		renderWithClient(<App />, ["/films"]);
+
+		await user.click(screen.getByTestId("people-link"));
+
+		expect(
+			screen.getByRole("heading", {
+				name: "People",
+			}),
+		).toBeInTheDocument();
+	});
+
+	it("navigates from People to Home using desktop nav", async () => {
+		renderWithClient(<App />, ["/people"]);
+
+		await user.click(screen.getByTestId("home-link"));
+
+		expect(
+			screen.getByRole("heading", {
+				name: "Home Page",
+			}),
+		).toBeInTheDocument();
+	});
+
+	it("navigates from Not Found to Home using desktop nav", async () => {
+		renderWithClient(<App />, ["/bad-route"]);
+
+		await user.click(screen.getByTestId("home-link"));
+
+		expect(
+			screen.getByRole("heading", {
+				name: "Home Page",
+			}),
 		).toBeInTheDocument();
 	});
 });
