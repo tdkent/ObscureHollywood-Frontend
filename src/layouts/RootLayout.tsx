339bc9f3f920987122defc2c3455a75f4ff069cd
Tim <timkentdev@gmail.com>
2026-04-16 20:01:23 -0700

    Feat: set flexible layout and create Footer component (#9)
    * create Footer component
    
    * create container div to fill screen. set main to fill available space

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router";
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 
 export default function RootLayout() {
 	return (
-		<>
+		<div className="h-screen flex flex-col">
 			<Header />
-			<main>
+			<main className="flex-1 lg:flex lg:flex-col lg:items-center">
 				<Outlet />
 			</main>
-			<footer></footer>
-		</>
+			<Footer />
+		</div>
 	);
 }
