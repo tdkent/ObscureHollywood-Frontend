21ecd91f38a007e129374b6f39fd97634bc758a9
Tim <timkentdev@gmail.com>
2026-04-16 20:32:23 -0700

    add global error component to app boundary

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter } from "react-router";
 import App from "@/App";
+import GlobalErrorBoundary from "@/GlobalError";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<ErrorBoundary fallback={<div>An error occurred!</div>}>
+		<ErrorBoundary
+			fallbackRender={({ error, resetErrorBoundary }) => (
+				<GlobalErrorBoundary error={error} reset={resetErrorBoundary} />
+			)}
+		>
 			<BrowserRouter>
 				<App />
 			</BrowserRouter>
