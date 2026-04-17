d6aaf99e60204427f3223cda7a93347385252eb3
Tim <timkentdev@gmail.com>
2026-04-16 22:21:20 -0700

    Chore: add and configure Tanstack Query (#11)
    * add tanstack query pkgs
    
    * config tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
@@ -11,6 +13,14 @@ if (!root) {
 	throw new Error("Could not find root element");
 }
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 1000 * 60 * 60 * 24, // 1d
+		},
+	},
+});
+
 createRoot(root).render(
 	<StrictMode>
 		<ErrorBoundary
@@ -19,7 +29,10 @@ createRoot(root).render(
 			)}
 		>
 			<BrowserRouter>
-				<App />
+				<QueryClientProvider client={queryClient}>
+					<App />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
 			</BrowserRouter>
 		</ErrorBoundary>
 	</StrictMode>,
