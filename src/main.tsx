4f578b0ca39d6da99bfb8adb034bb06d53f2b728
Tim <timkentdev@gmail.com>
2026-04-16 20:10:01 -0700

    add error boundary to application

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter } from "react-router";
 import App from "@/App";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<ErrorBoundary fallback={<div>An error occurred!</div>}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ErrorBoundary>
 	</StrictMode>,
 );
