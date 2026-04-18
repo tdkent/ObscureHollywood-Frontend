6078b649016e793e7f541a69d4c8b08f067314e4
Tim <timkentdev@gmail.com>
2026-04-18 13:08:28 -0700

    create options for paginated items select input

---
+++ src/lib/paginatedSortOptions.ts
@@ -0,0 +1,80 @@
+type SortValue =
+	| "firstNameAsc"
+	| "firstNameDesc"
+	| "lastNameAsc"
+	| "lastNameDesc"
+	| "nameAsc"
+	| "nameDesc"
+	| "yearAsc"
+	| "yearDesc";
+
+type Entity = "features" | "films" | "people" | "studios" | "tags";
+
+interface SelectOption {
+	label: string;
+	value: SortValue;
+}
+
+interface SortOption {
+	entity: Entity;
+	options: SelectOption[];
+}
+
+const firstNameAsc: SelectOption = {
+	label: "First Name (A-Z)",
+	value: "firstNameAsc",
+};
+
+const firstNameDesc: SelectOption = {
+	label: "First Name (Z-A)",
+	value: "firstNameDesc",
+};
+
+const lastNameAsc: SelectOption = {
+	label: "Last Name (A-Z)",
+	value: "lastNameAsc",
+};
+
+const lastNameDesc: SelectOption = {
+	label: "Last Name (Z-A)",
+	value: "lastNameDesc",
+};
+
+const nameAsc: SelectOption = {
+	label: "Name (A-Z)",
+	value: "nameAsc",
+};
+
+const nameDesc: SelectOption = {
+	label: "Name (Z-A)",
+	value: "nameDesc",
+};
+
+const yearAsc: SelectOption = {
+	label: "Year (Oldest-Newest)",
+	value: "yearAsc",
+};
+
+const yearDesc: SelectOption = {
+	label: "Year (Newest-Oldest)",
+	value: "yearDesc",
+};
+
+export const paginatedSortOptions: SortOption[] = [
+	{
+		entity: "features",
+		options: [nameAsc, nameDesc],
+	},
+	{
+		entity: "films",
+		options: [nameAsc, nameDesc, yearAsc, yearDesc],
+	},
+	{
+		entity: "people",
+		options: [firstNameAsc, firstNameDesc, lastNameAsc, lastNameDesc],
+	},
+	{
+		entity: "studios",
+		options: [nameAsc, nameDesc],
+	},
+];
