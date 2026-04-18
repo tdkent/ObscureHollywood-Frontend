25988ef277e426567d2a6915927e1a893f57684e
Tim <timkentdev@gmail.com>
2026-04-17 17:30:43 -0700

    add basic necessary route info to Films page and pass as props to Paginated component

--- src/pages/Films.page.tsx
+++ src/pages/Films.page.tsx
@@ -1,10 +1,15 @@
-import List from "@/components/shared/List";
+import { useLocation } from "react-router";
+import Paginated from "@/components/list/Paginated";
 
 export default function FilmsPage() {
+	const { pathname, search } = useLocation();
+
+	const route = `${pathname}${search}`;
+
 	return (
 		<div>
 			<h2>Films</h2>
-			<List />
+			<Paginated route={route} />
 		</div>
 	);
 }
