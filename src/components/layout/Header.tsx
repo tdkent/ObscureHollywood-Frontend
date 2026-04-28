4e6eb179f90c16b6509a01fe42e5f0b1736dcd99
Tim <timkentdev@gmail.com>
2026-04-28 09:45:09 -0700

    add limelight font and variable

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 		<header className="border-b">
 			<div className="flex flex-col py-4 px-6 gap-4">
 				<div className="flex justify-between items-center">
-					<span>Obscure Hollywood</span>
+					<span className="font-limelight text-xl">Obscure Hollywood</span>
 					<Navigation />
 				</div>
 				<Search />
