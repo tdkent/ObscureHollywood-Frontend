d822164543e4566bb15c5b6d01f9a6d0d864bc93
Tim <timkentdev@gmail.com>
2026-04-29 12:54:48 -0700

    conditionally render pagination links or span elements with aria attributes

--- src/components/list/PaginationLinks.tsx
+++ src/components/list/PaginationLinks.tsx
@@ -13,31 +13,53 @@ export default function PaginationLinks({
 	lastPage,
 	links,
 }: Props) {
+	const isFirstPage = currentPage === 1;
+	const isLastPage = !lastPage || currentPage === lastPage;
 	return (
 		<nav aria-label="Pagination" className="flex justify-center mt-12">
 			<ul className="join">
-				<li className="join-item btn">
-					<Link aria-label="Page 1" to={`?${links.first}`}>
-						1
-					</Link>
+				<li className={`join-item btn ${isFirstPage && "btn-disabled"}`}>
+					{isFirstPage ? (
+						<span aria-disabled>1</span>
+					) : (
+						<Link aria-label="Page 1" to={`?${links.first}`}>
+							1
+						</Link>
+					)}
 				</li>
-				<li className="join-item btn">
-					<Link aria-label="Previous page" to={`?${links.previous}`}>
-						<ChevronLeft className="size-4" />
-					</Link>
+				<li className={`join-item btn ${isFirstPage && "btn-disabled"}`}>
+					{isFirstPage ? (
+						<span aria-disabled>
+							<ChevronLeft className="size-4" />
+						</span>
+					) : (
+						<Link aria-label="Previous page" to={`?${links.previous}`}>
+							<ChevronLeft className="size-4" />
+						</Link>
+					)}
 				</li>
 				<li className="join-item btn btn-active">
-					<span>{currentPage}</span>
+					<span aria-current>{currentPage}</span>
 				</li>
-				<li className="join-item btn">
-					<Link aria-label="Next page" to={`?${links.next}`}>
-						<ChevronRight className="size-4" />
-					</Link>
+				<li className={`join-item btn ${isLastPage && "btn-disabled"}`}>
+					{isLastPage ? (
+						<span aria-disabled>
+							<ChevronRight className="size-4" />
+						</span>
+					) : (
+						<Link aria-label="Next page" to={`?${links.next}`}>
+							<ChevronRight className="size-4" />
+						</Link>
+					)}
 				</li>
-				<li className="join-item btn">
-					<Link aria-label={`Page ${lastPage || 1}`} to={`?${links.last}`}>
-						{lastPage || 1}
-					</Link>
+				<li className={`join-item btn ${isLastPage && "btn-disabled"}`}>
+					{isLastPage ? (
+						<span aria-disabled>{lastPage || 1}</span>
+					) : (
+						<Link aria-label={`Page ${lastPage || 1}`} to={`?${links.last}`}>
+							{lastPage || 1}
+						</Link>
+					)}
 				</li>
 			</ul>
 		</nav>
