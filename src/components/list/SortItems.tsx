f25c81c1ef73bbeb0ac816ac0e3c32c9fe768e37
Tim <timkentdev@gmail.com>
2026-04-28 21:28:13 -0700

    use daisy select class for sort select element

--- src/components/list/SortItems.tsx
+++ src/components/list/SortItems.tsx
@@ -35,11 +35,7 @@ export default function SortItems({
 	}
 
 	return (
-		<select
-			className="text-sm border grow rounded-sm px-2 bg-bg-accent"
-			id="sort-items"
-			onChange={handleSelect}
-		>
+		<select className="select" id="sort-items" onChange={handleSelect}>
 			{sortOption
 				? sortOption.options
 						.sort((a, b) => a.id - b.id)
