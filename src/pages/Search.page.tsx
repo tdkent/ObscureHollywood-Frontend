516832670cf3989ee6f4cc051b00b74f9f4a8591
Tim <timkentdev@gmail.com>
2026-04-30 20:05:17 -0700

    show search term on search page

--- src/pages/Search.page.tsx
+++ src/pages/Search.page.tsx
@@ -1,9 +1,15 @@
+import { useSearchParams } from "react-router";
 import Paginated from "@/components/list/Paginated";
 
 export default function SearchPage() {
+	const [searchParams] = useSearchParams();
+
+	const searchParam = searchParams.get("searchString");
+
 	return (
 		<div>
 			<h1 className="font-light text-2xl">Search Results</h1>
+			<p className="my-4 text-lg">"{searchParam}"</p>
 			<Paginated />
 		</div>
 	);
