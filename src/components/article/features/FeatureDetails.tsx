d8d04c09514af1bab6615e726627584dd29c03bd
Tim <timkentdev@gmail.com>
2026-05-05 09:20:35 -0700

    create article header element with title, subtitle, image

--- src/components/article/features/FeatureDetails.tsx
+++ src/components/article/features/FeatureDetails.tsx
@@ -1,3 +1,4 @@
+import Image from "@/components/shared/Image";
 import type { FeatureWithRelations } from "@/types/feature.interface";
 
 interface Props {
@@ -5,9 +6,21 @@ interface Props {
 }
 
 export default function FeatureDetails({ feature }: Props) {
+	const { name, slug, subtitle } = feature;
 	return (
-		<section>
-			<h1>{feature.name}</h1>
-		</section>
+		<div className="flex flex-col gap-8 my-2">
+			<header className="flex flex-col gap-4">
+				<div>
+					<h1 className="text-3xl font-normal">{name}</h1>
+					<h2 className="text-xl font-light">{subtitle}</h2>
+				</div>
+				<Image
+					altText={name}
+					fetchPriority="high"
+					lazyLoading="eager"
+					slug={slug}
+				/>
+			</header>
+		</div>
 	);
 }
--- src/pages/FeatureArticle.page.tsx
+++ src/pages/FeatureArticle.page.tsx
@@ -2,9 +2,9 @@ import FeatureArticle from "@/components/article/features/FeatureArticle";
 
 export default function FeatureArticlePage() {
 	return (
-		<div>
-			<span>Feature</span>
+		<>
+			<span className="text-sm font-light">Feature</span>
 			<FeatureArticle />
-		</div>
+		</>
 	);
 }
