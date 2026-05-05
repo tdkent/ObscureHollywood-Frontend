f4bad4596046c085f196fd9daab0058d8f331669
Tim <timkentdev@gmail.com>
2026-05-04 19:38:23 -0700

    update response image props and attributes

--- src/components/shared/Image.tsx
+++ src/components/shared/Image.tsx
@@ -1,45 +1,34 @@
 import { ImageOff, LoaderCircle } from "lucide-react";
 import { useState } from "react";
+import imgSrcSets from "@/lib/imgSrcSets";
 
 interface Props {
 	altText: string;
-	aspectRatio?: string;
-	fetchPriority?: "high";
+	containerStyles?: string;
+	fetchPriority?: "high" | "low";
 	imgStyles?: string;
-	imgUrl: string;
-	isHeader?: boolean;
-	lazy?: boolean;
+	lazyLoading?: "eager" | "lazy";
 	sizes?: string;
-	srcSets: SrcSets;
-}
-
-interface SrcSets {
-	avif: string;
-	webp: string;
+	slug: string;
 }
 
 /** Responsive picture element loads images based on display/device. */
 export default function Image({
 	altText,
-	aspectRatio,
+	containerStyles = "w-full aspect-[7/5]",
 	fetchPriority,
 	imgStyles,
-	imgUrl,
-	isHeader,
-	lazy,
-	sizes,
-	srcSets,
+	lazyLoading = "lazy",
+	sizes = "100vw",
+	slug,
 }: Props) {
 	const [loading, setLoading] = useState(true);
 	const [error, setError] = useState(false);
 
-	// If no AR specify one to avoid layout shift after load or error
-	const loadingAspectRatio = loading || error ? "aspect-5/3" : "aspect-auto";
+	const { avif, jpeg, webp } = imgSrcSets(slug);
 
 	return (
-		<div
-			className={`w-full overflow-hidden bg-[#f9f9f9] ${aspectRatio || loadingAspectRatio}`}
-		>
+		<div className={`overflow-hidden bg-bg-accent ${containerStyles}`}>
 			{loading && (
 				<div className="flex items-center justify-center gap-2 text-sm p-4">
 					<LoaderCircle className="size-6 stroke-1 animate-spin" />
@@ -52,23 +41,21 @@ export default function Image({
 					The image failed to load.
 				</div>
 			)}
-			<picture
-				className={`${isHeader ? "flex" : "block"} w-full h-full ${error ? "hidden" : ""}`}
-			>
-				<source srcSet={srcSets.avif} sizes={sizes} type="image/avif" />
-				<source srcSet={srcSets.webp} sizes={sizes} type="image/webp" />
+			<picture className={`w-full h-full ${error ? "hidden" : ""}`}>
+				<source srcSet={avif} sizes={sizes} type="image/avif" />
+				<source srcSet={webp} sizes={sizes} type="image/webp" />
 				<img
-					src={imgUrl}
 					alt={altText}
-					sizes={sizes}
-					loading={lazy ? "lazy" : "eager"}
-					className={`object-cover w-full h-auto ${imgStyles}`}
-					onLoad={() => setLoading(false)}
+					className={`object-cover w-full h-full ${imgStyles}`}
+					fetchPriority={fetchPriority}
+					loading={lazyLoading}
 					onError={() => {
 						setLoading(false);
 						setError(true);
 					}}
-					fetchPriority={fetchPriority}
+					onLoad={() => setLoading(false)}
+					sizes={sizes}
+					srcSet={jpeg}
 				/>
 			</picture>
 		</div>
