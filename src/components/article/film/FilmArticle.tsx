1696b5ce4a51368891eeac10f09128191430a09c
Tim <timkentdev@gmail.com>
2026-05-08 18:29:04 -0700

    create metadata to render Film article header dl

--- src/components/article/film/FilmArticle.tsx
+++ src/components/article/film/FilmArticle.tsx
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { useLocation, useParams } from "react-router";
 import httpRequest from "@/api/httpRequest";
-import FilmDetails from "@/components/article/film/FilmDetails";
+import ArticleHeader from "@/components/article/ArticleHeader";
+import DescriptionList from "@/components/shared/DescriptionList";
 import DisplayError from "@/components/shared/DisplayError";
 import Loading from "@/components/shared/Loading";
 import type { FilmWithRelations } from "@/types/film.interface";
-import type { Entity } from "@/types/ui.interface";
+import type { DlMetadata, Entity } from "@/types/ui.interface";
 
 export default function FilmArticle() {
 	const { slug } = useParams();
@@ -21,9 +22,87 @@ export default function FilmArticle() {
 	if (isPending) return <Loading />;
 	if (error) return <DisplayError error={error} />;
 
+	const {
+		article,
+		filmTags,
+		name,
+		personFilms,
+		releaseYear,
+		slug: filmSlug,
+		studio,
+	} = data as FilmWithRelations;
+
+	// Filter and join in case of multiple directors
+	const directors: DlMetadata["description"] = personFilms
+		.filter((person) => person.role === "director")
+		.map((person) => {
+			return {
+				label: person.person.name,
+				href: `/people/${person.person.slug}`,
+			};
+		});
+
+	const writers = personFilms
+		.filter((person) => person.role === "writer")
+		.map((person) => {
+			return {
+				label: person.person.name,
+				href: `/people/${person.person.slug}`,
+			};
+		});
+
+	const actors = personFilms
+		.filter((person) => person.role === "actor")
+		.sort((a, b) => a.castPosition - b.castPosition)
+		.map((person) => {
+			return {
+				label: person.person.name,
+				href: `/people/${person.person.slug}`,
+			};
+		});
+
+	const tags = filmTags.map((ft) => {
+		return {
+			label: ft.tag.name,
+			href: `/tags/${ft.tag.slug}`,
+		};
+	});
+
+	const metadata: DlMetadata[] = [
+		{
+			title: "Release Year",
+			description: {
+				label: releaseYear,
+			},
+		},
+		{
+			title: "Studio",
+			description: {
+				label: studio.name,
+				href: `/studios/${studio.slug}`,
+			},
+		},
+		{
+			title: `Director${directors.length > 1 ? "s" : ""}`,
+			description: directors,
+		},
+		{
+			title: `Screenwriter${writers.length > 1 ? "s" : ""}`,
+			description: writers,
+		},
+		{
+			title: "Cast",
+			description: actors,
+		},
+		{
+			title: "Tags",
+			description: tags,
+		},
+	];
+
 	return (
-		<div>
-			<FilmDetails film={data as FilmWithRelations} />
-		</div>
+		<ArticleHeader name={name} slug={filmSlug}>
+			<DescriptionList metadata={metadata} />
+		</ArticleHeader>
 	);
 }
