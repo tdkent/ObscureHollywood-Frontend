f66b5e1d42e45f9939fcaeeba868e33ce8b93217
Tim <timkentdev@gmail.com>
2026-05-11 21:27:05 -0700

    add section text and recent article list items

--- src/components/home/Section.tsx
+++ src/components/home/Section.tsx
@@ -1,13 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router";
+import httpRequest from "@/api/httpRequest";
+import DisplayError from "@/components/shared/DisplayError";
+import Image from "@/components/shared/Image";
+import Loading from "@/components/shared/Loading";
+import type { PartialListItem } from "@/types/paginated-response.interface";
+
 interface Props {
+	imgs: { name: string; slug: string }[];
+	route: "features" | "films" | "people";
 	text: string;
 	title: string;
 }
 
-export default function Section({ text, title }: Props) {
+export default function Section({ imgs, route, text, title }: Props) {
+	const { data, error, isPending } = useQuery({
+		queryKey: [route, "recent"],
+		queryFn: () => httpRequest(`/${route}/recent`),
+	});
+
+	if (isPending) return <Loading />;
+	if (error) return <DisplayError error={error} />;
+
+	const recentArticles = data as PartialListItem[];
+
 	return (
-		<section className="py-8">
-			<h2 className="text-3xl font-bold">{title}</h2>
-			<p>{text}</p>
+		<section className="py-4">
+			<div className="flex flex-nowrap">
+				{imgs.map(({ name, slug }) => {
+					return <Image key={slug} altText={name} slug={slug} />;
+				})}
+			</div>
+			<div className="flex flex-col text-center gap-4 p-6">
+				<h2 className="text-3xl font-bold">{title}</h2>
+				<p>{text}</p>
+				<div className="flex flex-col gap-4">
+					<h3 className="font-bodini-moda italic text-2xl font-bold">
+						Recent features:
+					</h3>
+					<ul className="flex flex-col gap-6">
+						{recentArticles.map(({ id, name, slug }) => {
+							return (
+								<li
+									className="font-bold text-lg rounded-t-2xl  border overflow-hidden  max-lg:last:hidden"
+									key={id}
+								>
+									<Link to={`/${route}/${slug}`}>
+										<Image altText={name} slug={slug} />
+										{name}
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
 		</section>
 	);
 }
