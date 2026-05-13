3db8e791b480e4f939747e6a756c57cc4b60d6d8
Tim <timkentdev@gmail.com>
2026-05-12 17:03:59 -0700

    add css variants to keep splash styles consistent in light/dark mode

--- src/components/layout/Search.tsx
+++ src/components/layout/Search.tsx
@@ -1,8 +1,10 @@
 import { Search as SearchIcon } from "lucide-react";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
 export default function Search() {
 	const navigate = useNavigate();
+	const { pathname } = useLocation();
+	const isHome = pathname === "/";
 
 	function handleSearch(formData: FormData) {
 		const searchString = formData.get("search");
@@ -19,20 +21,20 @@ export default function Search() {
 		<search>
 			<form
 				action={handleSearch}
-				className="border rounded-lg flex h-12 overflow-hidden"
+				className="border border-dark-border rounded-lg flex h-12 overflow-hidden"
 				id="search"
 			>
 				<input
-					className="grow h-full outline-none px-3"
+					className={`grow h-full outline-none px-3 text-xl ${isHome ? "bg-black/30" : "bg-bg-accent"}`}
 					type="search"
 					name="search"
 				/>
 				<button
-					className="h-full flex items-center border-l bg-bg-accent px-3"
+					className={`h-full flex items-center border-l border-dark-border px-3 ${isHome ? "bg-black/10" : "bg-bg-accent"}`}
 					type="submit"
 				>
 					<span className="sr-only">Search</span>
-					<SearchIcon className="stroke-1" />
+					<SearchIcon className="stroke-1 size-6 text-text-white" />
 				</button>
 			</form>
 		</search>
