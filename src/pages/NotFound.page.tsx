ed8e5e19d69a6af82d58cbce80734ba8ccb64c53
Tim <timkentdev@gmail.com>
2026-05-13 20:13:40 -0700

    update 404 page

--- src/pages/NotFound.page.tsx
+++ src/pages/NotFound.page.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router";
+
 export default function NotFoundPage() {
 	return (
-		<div className="page-margins">
-			<h1>404 - Page Not Found</h1>
+		<div className="page-margins px-6 text-center flex flex-col items-center gap-6">
+			<h1 className="font-limelight">
+				<span className="block text-8xl">404</span>
+				<span className="block">Page Not Found</span>
+			</h1>
+
+			<Link className="text-link w-fit" to="/">
+				Go to home page
+			</Link>
 		</div>
 	);
 }
