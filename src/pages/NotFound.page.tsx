a0889646d4433bbe6219b960bf4635c9b11ac45e
Tim <timkentdev@gmail.com>
2026-05-18 22:17:42 -0700

    fix: grow main content

--- src/pages/NotFound.page.tsx
+++ src/pages/NotFound.page.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function NotFoundPage() {
 	return (
 		<div className="page-margins bg-content px-6 text-center flex flex-col items-center gap-6">
-			<h1 className="font-limelight text-secondary-text">
+			<h1 className="font-limelight text-secondary-text py-10">
 				<span className="block text-8xl">404</span>
 				<span className="block">Page Not Found</span>
 			</h1>
