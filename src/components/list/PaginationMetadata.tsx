4b0e2809c698adac5d96529469ae7d4984ddd890
Tim <timkentdev@gmail.com>
2026-05-20 12:30:48 -0700

    fix: only show selected tags on /films page

--- src/components/list/PaginationMetadata.tsx
+++ src/components/list/PaginationMetadata.tsx
@@ -1,6 +1,6 @@
 import { X } from "lucide-react";
 import type { Dispatch, SetStateAction } from "react";
-import { useSearchParams } from "react-router";
+import { useLocation, useSearchParams } from "react-router";
 import type { PaginatedResponse } from "@/types/paginated-response.interface";
 
 interface Props {
@@ -21,6 +21,9 @@ export default function PaginationMetadata({
 	tags,
 }: Props) {
 	const [_, setSearchParams] = useSearchParams();
+	const { pathname } = useLocation();
+
+	const showTags = pathname === "/films" && tags.length;
 
 	/** Remove selected tag and reconstruct search url. */
 	function handleClick(selected: string) {
@@ -45,7 +48,7 @@ export default function PaginationMetadata({
 				<p>No results found.</p>
 			)}
 
-			{tags.length ? (
+			{showTags && (
 				<div className="flex gap-2">
 					Tags:
 					<ul className="flex flex-col gap-2">
@@ -65,7 +68,7 @@ export default function PaginationMetadata({
 						})}
 					</ul>
 				</div>
-			) : null}
+			)}
 		</div>
 	);
 }
