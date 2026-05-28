3e2b7d01106ecd1586377c54c6660a7798b64af8
Tim <timkentdev@gmail.com>
2026-05-28 11:32:46 -0700

    Refactor: quiz form components (#48)
    * create radio group component
    
    * create results alert component
    
    * fix: filter extra escaped double-qoutes out of parsed html strings

---
+++ src/components/article/quiz/RadioGroup.tsx
@@ -0,0 +1,33 @@
+import type { ControllerRenderProps } from "react-hook-form";
+import type { FormInputs } from "@/types/ui.interface";
+
+interface Props {
+	answerText: string;
+	field: ControllerRenderProps<FormInputs, string>;
+	idx: number;
+	isPending: boolean;
+	showResults: boolean;
+}
+
+export default function RadioGroup({
+	answerText,
+	field,
+	idx,
+	isPending,
+	showResults,
+}: Props) {
+	const inputValue = `${idx + 1}`;
+	return (
+		<label className={`flex gap-4 items-center text-base cursor-pointer w-fit`}>
+			<input
+				checked={field.value === inputValue}
+				className="radio bg-content-alt checked:bg-gold"
+				disabled={isPending || showResults}
+				type="radio"
+				value={inputValue}
+				onChange={() => field.onChange(inputValue)}
+			/>
+			{answerText}
+		</label>
+	);
+}
