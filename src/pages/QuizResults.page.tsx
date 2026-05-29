3b8564dc3d5784df32c6d26297cc39216d21b565
Tim <timkentdev@gmail.com>
2026-05-28 20:22:47 -0700

    create basic my-quizzes page and add to routes

---
+++ src/pages/QuizResults.page.tsx
@@ -0,0 +1,9 @@
+export default function QuizResultsPage() {
+	return (
+		<div className="page-margins bg-content">
+			<div className="my-4 px-6 sm:px-12">
+				<h1 className="text-3xl md:text-4xl">My Quizzes</h1>
+			</div>
+		</div>
+	);
+}
