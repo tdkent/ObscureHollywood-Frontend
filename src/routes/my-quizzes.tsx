7ad3882e867ae9643b5f7412ec352d1e50d7bd26
Tim <timkentdev@gmail.com>
2026-06-02 21:48:22 -0700

    render my-quizzes page

--- src/routes/my-quizzes.tsx
+++ src/routes/my-quizzes.tsx
@@ -1,9 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ArticleHeader from "@/components/article/ArticleHeader";
+import UserQuizzes from "@/components/my-quizzes/UserQuizzes";
+import { getUserId } from "@/lib/utils/getUserId";
 
 export const Route = createFileRoute("/my-quizzes")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/my-quizzes"!</div>;
+	const userId = getUserId();
+
+	return (
+		<div className="page-margins bg-content">
+			<div className="flex flex-col my-4 gap-6 sm:gap-12">
+				<ArticleHeader name="My Quizzes" />
+				{userId ? (
+					<UserQuizzes userId={userId} />
+				) : (
+					<p className="px-6 sm:px-12">
+						You have not completed any quizzes yet.
+					</p>
+				)}
+			</div>
+		</div>
+	);
 }
