f400192eb6eef0bc23abddb7f0d1345f3d1eff48
Tim <timkentdev@gmail.com>
2026-06-03 18:34:28 -0700

    remove react router pkg

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import App from "@/App";
 
 const root = document.getElementById("root");
@@ -11,8 +10,6 @@ if (!root) {
 
 createRoot(root).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<App />
 	</StrictMode>,
 );
