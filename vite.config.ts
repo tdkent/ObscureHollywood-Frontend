0347395269e3eda816b4cf845c88f1034526c837
Tim <timkentdev@gmail.com>
2026-06-05 10:07:29 -0700

    Deploy: configure Netlify deployment (#56)
    * add netlify-tanstack-start pkg
    
    * fix: force bundle react-parser for ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
@@ -6,7 +7,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [tanstackStart(), react(), tailwindcss()],
+	plugins: [tanstackStart(), react(), tailwindcss(), netlify()],
 	build: {
 		assetsDir: "static",
 	},
@@ -14,5 +15,9 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
+	},
+	ssr: {
+		//? Force Vite to bundle for SSR
+		noExternal: ["html-react-parser"],
 	},
 });
