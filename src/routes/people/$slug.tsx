b4d9b13888f6139660b6add90264dd9fd23ed126
Tim <timkentdev@gmail.com>
2026-06-05 17:51:10 -0700

    add dynamic titletag to feature, film, person article pages

--- src/routes/people/$slug.tsx
+++ src/routes/people/$slug.tsx
@@ -3,18 +3,30 @@ import { Suspense } from "react";
 import PersonArticle from "@/components/article/person/PersonArticle";
 import DetailPage from "@/components/layout/containers/DetailPage";
 import Loading from "@/components/shared/Loading";
+import type { PersonWithRelations } from "@/types/person.interface";
 import { articleQueryOptions } from "@/util/articleQueryOptions";
 
 export const Route = createFileRoute("/people/$slug")({
 	loader: async ({ context, params }) => {
-		await context.queryClient.ensureQueryData(
+		const person = (await context.queryClient.ensureQueryData(
 			articleQueryOptions({
 				route: "people",
 				slug: params.slug,
 			}),
-		);
+		)) as PersonWithRelations;
+
+		return { person };
 	},
 	component: RouteComponent,
+	head: ({ loaderData }) => ({
+		meta: [
+			{
+				title: loaderData
+					? `${loaderData.person.name} - Obscure Hollywood`
+					: "Not Found - Obscure Hollywood",
+			},
+		],
+	}),
 });
 
 function RouteComponent() {
