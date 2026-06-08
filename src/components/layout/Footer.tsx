29d07adc6d093cc49c4dc79e5bfa408cf390346c
Tim <timkentdev@gmail.com>
2026-06-08 12:51:16 -0700

    Style: update footer flex layout (#63)

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,11 +3,11 @@ import { navLinks } from "@/lib/navLinks";
 
 export default function Footer() {
 	return (
-		<footer className="border-t bg-footer text-center font-thin pt-6 pb-12 flex flex-col gap-10 sm:pt-10 sm:pb-16">
-			<ul className="flex flex-col gap-4 text-sm sm:text-base">
+		<footer className="border-t bg-footer text-center font-thin pt-6 pb-12 flex flex-col items-center gap-10 sm:pt-10 sm:pb-16">
+			<ul className="flex flex-wrap justify-evenly gap-4 text-base w-full max-w-4xl sm:flex-nowrap sm:divide-x">
 				{navLinks.map((link) => {
 					return (
-						<li key={link.label}>
+						<li key={link.label} className="basis-1/3">
 							<Link to={link.href}>{link.label}</Link>
 						</li>
 					);
