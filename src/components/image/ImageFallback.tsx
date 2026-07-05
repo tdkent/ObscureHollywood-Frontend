c0fc612955c1a847e9f65fc0a16a3b0751068a92
Tim <timkentdev@gmail.com>
2026-07-05 12:13:04 -0700

    conditionally load fallback imgs in fallback component

--- src/components/image/ImageFallback.tsx
+++ src/components/image/ImageFallback.tsx
@@ -10,15 +10,19 @@ export default function ImageFallback({
 	personGender,
 }: ImageFallback) {
 	return (
-		<div className="flex items-center justify-center gap-2 text-sm p-4 h-full">
+		<div>
 			{isHeader ? (
 				personGender ? (
-					<span>{personGender}</span>
+					<img
+						src={`/img/fallback-${personGender}.jpg`}
+						alt={`silhouette of a person`}
+						className="object-cover w-full h-full"
+					/>
 				) : (
-					<>
+					<div className="flex items-center justify-center gap-2 text-sm p-4 aspect-7/5 sm:text-base">
 						<CircleAlert className="stroke-1" />
 						<span>No image available!</span>
-					</>
+					</div>
 				)
 			) : (
 				<ImageOff className="stroke-1 stroke-text/50" />
