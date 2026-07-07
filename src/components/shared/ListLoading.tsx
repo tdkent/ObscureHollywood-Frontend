d0c18f25448a7fd69d03bb96f5315ef8e3c3829d
Tim <timkentdev@gmail.com>
2026-07-06 17:30:49 -0700

    add card loading variant

--- src/components/shared/ListLoading.tsx
+++ src/components/shared/ListLoading.tsx
@@ -1,8 +1,27 @@
 import { createEmptyList } from "@/util/createEmptyList";
 
 export default function ListLoading() {
-	const list = createEmptyList(10);
+	const list = createEmptyList(12);
 	const useCard = localStorage.getItem("useCardTheme");
+
+	if (useCard) {
+		return (
+			<div className="flex flex-col sm:flex-row sm:flex-wrap">
+				{list.map((item) => {
+					return (
+						<div
+							key={item}
+							className="flex flex-col gap-3 items-center basis-full px-2 py-4 sm:basis-1/2 sm:w-2/5 md:py-6 lg:basis-1/3"
+						>
+							<div className="skeleton w-full aspect-7/5 rounded-lg dark:bg-content-alt"></div>
+							<div className="skeleton h-6 w-3/5 rounded-sm dark:bg-content-alt"></div>
+							<div className="skeleton h-6 w-2/5 rounded-sm dark:bg-content-alt"></div>
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 
 	return (
 		<div className="flex flex-col gap-4">
